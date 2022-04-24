6f86a294a2d932d278f782886247e07b2102536f
RutvikChandla <rutvik.c@browserstack.com>
2022-04-24 19:53:51 +0530

    suite code

--- src/test/java/com/browserstack/suite/SuiteTest09.java
+++ src/test/java/com/browserstack/suite/SuiteTest09.java
@@ -12,14 +12,22 @@ public class SuiteTest09 extends BrowserStackTestNGTest {
 
     @Test
     public void test_09() throws Exception {
-        driver.get("https://www.google.com/ncr");
-        Thread.sleep(3000);
-        WebElement element = driver.findElement(By.name("q"));
-        element.sendKeys("BrowserStack Test 09");
-        Thread.sleep(3000);
-        element.submit();
-        Thread.sleep(3000);
-
-        Assert.assertEquals("BrowserStack Test 09 - Google Search", driver.getTitle());
+    	// navigate to bstackdemo
+        driver.get("https://www.bstackdemo.com");
+        
+        // Check the title
+        Assert.assertTrue(driver.getTitle().matches("StackDemo"));
+        
+        // Save the text of the product for later verify
+        String productOnScreenText = driver.findElement(By.xpath("//*[@id=\"1\"]/p")).getText();
+        // Click on add to cart button
+        driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]")).click();
+        
+        // See if the cart is opened or not
+        Assert.assertTrue(driver.findElement(By.cssSelector(".float\\-cart__content")).isDisplayed());
+        
+        // Check the product inside the cart is same as of the main page
+        String productOnCartText = driver.findElement(By.xpath("//*[@id=\"__next\"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]")).getText();
+        Assert.assertEquals(productOnScreenText, productOnCartText);
     }
 }
