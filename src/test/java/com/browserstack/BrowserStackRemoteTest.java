87304c65de517a2f069745e7ddc8f745e3b1fdec
kamal-kaur04 <38219887+kamal-kaur04@users.noreply.github.com>
2022-06-07 17:09:23 +0530

    removed unused imports

--- src/test/java/com/browserstack/BrowserStackRemoteTest.java
+++ src/test/java/com/browserstack/BrowserStackRemoteTest.java
@@ -1,24 +1,12 @@
 package com.browserstack;
 
-import java.io.FileReader;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.Map;
 
-import com.browserstack.local.Local;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ITestContext;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 
 public class BrowserStackRemoteTest {
     public WebDriver driver;
@@ -28,7 +16,7 @@ public class BrowserStackRemoteTest {
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         driver = new RemoteWebDriver(
-                new URL("https://hub-cloud.browserstack.com/wd/hub"), capabilities);
+                new URL("https://hub.browserstack.com/wd/hub"), capabilities);
         }
 
     @AfterMethod(alwaysRun = true)
