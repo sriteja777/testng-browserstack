2a79e83d5463983a0a9c00b4da2a7642a7f8bb1f
kamal-kaur04 <38219887+kamal-kaur04@users.noreply.github.com>
2022-06-07 16:57:15 +0530

    added sample tests to run with java sdk

--- src/test/java/com/browserstack/BrowserStackRemoteTest.java
+++ src/test/java/com/browserstack/BrowserStackRemoteTest.java
@@ -24,93 +24,16 @@ public class BrowserStackRemoteTest {
     public WebDriver driver;
 
     @BeforeMethod(alwaysRun = true)
-    @org.testng.annotations.Parameters(value = { "profile", "platform" })
     @SuppressWarnings("unchecked")
-    public void setUp(@Optional("") String profile, String platform) throws Exception {
-        JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("config/browserstack.conf.json"));
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        JSONArray platforms = (JSONArray) config.get("platforms");
-
-        Map<String, String> envCapabilities = (Map<String, String>) platforms.get(Integer.parseInt(platform));
-        Iterator it = envCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-        }
-
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-        it = commonCapabilities.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry pair = (Map.Entry) it.next();
-            if (capabilities.getCapability(pair.getKey().toString()) == null) {
-                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-            }
-        }
-
-        if (!profile.equals("")) {
-            Map<String, Map<String, String>> profiles = (Map<String, Map<String, String>>) (config.get("profiles"));
-            Map<String, ?> profileMap = profiles.get(profile);
-            it = profileMap.entrySet().iterator();
-            while (it.hasNext()) {
-                Map.Entry pair = (Map.Entry) it.next();
-                if (capabilities.getCapability(pair.getKey().toString()) == null) {
-                    if(pair.getKey().toString().equals("localOptions")) {
-                        Map<String, String> localOptionsMap = (Map<String, String>) profileMap.get(pair.getKey().toString());
-                        if (localOptionsMap.get("localIdentifier") != null) {
-                            capabilities.setCapability("browserstack.localIdentifier", localOptionsMap.get("localIdentifier"));
-                        }
-                    } else {
-                        capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-                    }
-                }
-            }
-        }
-
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if (username == null) {
-            username = (String) config.get("user");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if (accessKey == null) {
-            accessKey = (String) config.get("key");
-        }
-
         driver = new RemoteWebDriver(
-                new URL("https://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
+                new URL("https://hub-cloud.browserstack.com/wd/hub"), capabilities);
         }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult testResult, ITestContext testContext) throws Exception {
-        String testName = testResult.getTestClass().getName() + "-" + testResult.getMethod().getMethodName();
-        markTestName(testName, driver);
-        if (testResult.isSuccess()) {
-            markTestStatus("passed", "", driver);
-        } else if (testResult.getThrowable().getMessage() != null) {
-            String testMessage = testResult.getThrowable().getMessage().replaceAll("\n", " ");
-            markTestStatus("failed", testMessage, driver);
-        } else {
-            markTestStatus("failed", "", driver);
-        }
+    public void tearDown() throws Exception {
         driver.quit();
-    }
-
-    // This method accepts the status, reason and WebDriver instance and marks the test on BrowserStack
-    public static void markTestStatus(String status, String reason, WebDriver driver) {
-        final JavascriptExecutor jse = (JavascriptExecutor) driver;
-        JSONObject executorObject = new JSONObject();
-        JSONObject argumentsObject = new JSONObject();
-        argumentsObject.put("status", status);
-        argumentsObject.put("reason", reason);
-        executorObject.put("action", "setSessionStatus");
-        executorObject.put("arguments", argumentsObject);
-        jse.executeScript(String.format("browserstack_executor: %s", executorObject));
-    }
-
-    public static void markTestName(String name, WebDriver driver) {
-        final JavascriptExecutor jse = (JavascriptExecutor) driver;
-        jse.executeScript("browserstack_executor: {\"action\": \"setSessionName\", \"arguments\": {\"name\": \"" + name + "\"}}");
     }
 }
 
