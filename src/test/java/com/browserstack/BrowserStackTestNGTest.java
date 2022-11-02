addae206cab947952568660d6786dab05cf190ae
Kamalpreet Kaur <kmlkaur73@gmail.com>
2022-11-02 11:36:00 +0530

    Modify Local Spawning Logic to fix parallel execution

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -13,31 +13,65 @@ import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 public class BrowserStackTestNGTest {
     public WebDriver driver;
-    private Local l;
+    private static Local l;
+    private static JSONObject config;
+    private static Map<String, Object> commonCapabilities;
+    private static String username;
+    private static String accessKey;
+
+    @BeforeSuite(alwaysRun=true)
+    @Parameters(value = { "config" })
+    public void beforeSuite(String config_file) throws Exception {
+        JSONParser parser = new JSONParser();
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        HashMap bstackOptionsMap = (HashMap) commonCapabilities.get("bstack:options");
+
+
+        username = System.getenv("BROWSERSTACK_USERNAME");
+        if (username == null) {
+            username = (String) config.get("user");
+        }
+
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if (accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+		try {
+			if ((bstackOptionsMap.get("local") != null &&
+					bstackOptionsMap.get("local").toString().equalsIgnoreCase("true") && (l == null))) {
+				l = new Local();
+				Map<String, String> options = new HashMap<String, String>();
+				options.put("key", accessKey);
+				l.start(options);
+			}
+		} catch (Exception e) {
+			System.out.println("Error while start local - " + e);
+		}
+    }
 
     @BeforeMethod(alwaysRun = true)
     @org.testng.annotations.Parameters(value = { "config", "environment" })
     @SuppressWarnings("unchecked")
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
         JSONObject envs = (JSONObject) config.get("environments");
 
         MutableCapabilities capabilities = new MutableCapabilities();
 
-        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
+        Map<String, Object> envCapabilities = (Map<String, Object>) envs.get(environment);
         Iterator it = envCapabilities.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
             capabilities.setCapability(pair.getKey().toString(), pair.getValue());
         }
 
-        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
         it = commonCapabilities.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
@@ -50,19 +84,20 @@ public class BrowserStackTestNGTest {
             }
         }
 
-        String username = System.getenv("BROWSERSTACK_USERNAME");
+        username = System.getenv("BROWSERSTACK_USERNAME");
         if (username == null) {
             username = (String) config.get("user");
         }
 
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
         if (accessKey == null) {
             accessKey = (String) config.get("key");
         }
 
         if (capabilities.getCapability("bstack:options") != null) {
             HashMap bstackOptionsMap = (HashMap) capabilities.getCapability("bstack:options");
-            if ((bstackOptionsMap.get("local") != null && bstackOptionsMap.get("local").toString().equalsIgnoreCase("true")) || (capabilities.getCapability("browserstack.local") != null && capabilities.getCapability("browserstack.local").toString().equalsIgnoreCase("true"))) {
+            if ((bstackOptionsMap.get("local") != null &&
+                    bstackOptionsMap.get("local").toString().equalsIgnoreCase("true") && (l == null || !l.isRunning()))) {
                 l = new Local();
                 Map<String, String> options = new HashMap<String, String>();
                 options.put("key", accessKey);
@@ -76,10 +111,12 @@ public class BrowserStackTestNGTest {
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
+    public void tearDown() {
         driver.quit();
-        if (l != null) {
-            l.stop();
-        }
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() throws Exception {
+        if (l != null) l.stop();
     }
 }
