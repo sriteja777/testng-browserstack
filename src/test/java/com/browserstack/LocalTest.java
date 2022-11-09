7c4e33c19ca600f9729dea4fcfc363fce11afc4d
Kamalpreet Kaur <kmlkaur73@gmail.com>
2022-11-09 13:49:11 +0530

    revert removal of throwing exception

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class LocalTest extends BrowserStackRemoteTest {
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         driver.get("http://bs-local.com:45454/");
 
         Assert.assertTrue(driver.getTitle().contains("BrowserStack Local"));
