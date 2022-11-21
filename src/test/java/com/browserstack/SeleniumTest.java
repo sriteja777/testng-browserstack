9e54bdfd13d7101fcc4ee68701f56eeda952331e
maxmattone <max.m@browserstack.com>
2022-11-21 14:30:09 +0000

    revert pom.xml remove useless imports

--- src/test/java/com/browserstack/SeleniumTest.java
+++ src/test/java/com/browserstack/SeleniumTest.java
@@ -3,9 +3,6 @@ package com.browserstack;
 import java.net.URL;
 import java.util.HashMap;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
