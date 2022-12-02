ff32314d81ca9056c39157573f5ee2d545c21761
Kamalpreet Kaur <kmlkaur73@gmail.com>
2022-12-02 17:06:09 +0530

    sync selenium-4 branch with master branch

--- src/test/java/com/browserstack/BrowserStackTestNGTest.java
+++ src/test/java/com/browserstack/BrowserStackTestNGTest.java
@@ -10,95 +10,103 @@ import com.browserstack.local.Local;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.*;
 
 public class BrowserStackTestNGTest {
     public WebDriver driver;
-    private Local bsLocal;
+    private static Local l;
+    private static JSONObject config;
+    private static Map<String, Object> commonCapabilities;
+    private static String username;
+    private static String accessKey;
 
-    @BeforeMethod(alwaysRun=true)
-    @org.testng.annotations.Parameters(value={"config", "environment"})
+    @BeforeSuite(alwaysRun=true)
+    @Parameters(value = { "config" })
+    public void beforeSuite(String config_file) throws Exception {
+        JSONParser parser = new JSONParser();
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        HashMap bstackOptionsMap = (HashMap) commonCapabilities.get("bstack:options");
+
+
+        username = System.getenv("BROWSERSTACK_USERNAME");
+        if (username == null) {
+            username = (String) config.get("user");
+        }
+
+        accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if (accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+        try {
+            if ((bstackOptionsMap.get("local") != null &&
+                    bstackOptionsMap.get("local").toString().equalsIgnoreCase("true") && (l == null))) {
+                l = new Local();
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("key", accessKey);
+                l.start(options);
+            }
+        } catch (Exception e) {
+            System.out.println("Error while start local - " + e);
+        }
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    @org.testng.annotations.Parameters(value = { "config", "environment" })
     @SuppressWarnings("unchecked")
     public void setUp(String config_file, String environment) throws Exception {
         JSONParser parser = new JSONParser();
-        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
         JSONObject envs = (JSONObject) config.get("environments");
 
         MutableCapabilities capabilities = new MutableCapabilities();
 
         Map<String, Object> envCapabilities = (Map<String, Object>) envs.get(environment);
-        Iterator<Map.Entry<String, Object>> it = envCapabilities.entrySet().iterator();
+        Iterator it = envCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry<String, Object> pair = (Map.Entry<String, Object>) it.next();
+            Map.Entry pair = (Map.Entry) it.next();
             capabilities.setCapability(pair.getKey().toString(), pair.getValue());
         }
 
-        Map<String, Object> commonCapabilities = (Map<String, Object>) config.get("capabilities");
+        commonCapabilities = (Map<String, Object>) config.get("capabilities");
         it = commonCapabilities.entrySet().iterator();
         while (it.hasNext()) {
-            Map.Entry<String, Object> pair = (Map.Entry<String, Object>) it.next();
-            Object envData = capabilities.getCapability(pair.getKey().toString());
-            Object resultData = pair.getValue();
-            if (envData != null && envData.getClass() == JSONObject.class) {
-                ((JSONObject) resultData).putAll((JSONObject) envData);
+            Map.Entry pair = (Map.Entry) it.next();
+            if (capabilities.getCapability(pair.getKey().toString()) == null) {
+                capabilities.setCapability(pair.getKey().toString(), pair.getValue());
+            } else if (pair.getKey().toString().equalsIgnoreCase("bstack:options")) {
+                HashMap bstackOptionsMap = (HashMap) pair.getValue();
+                bstackOptionsMap.putAll((HashMap) capabilities.getCapability("bstack:options"));
+                capabilities.setCapability(pair.getKey().toString(), bstackOptionsMap);
             }
-            capabilities.setCapability(pair.getKey().toString(), resultData);
-        }
-
-        String username = System.getenv("BROWSERSTACK_USERNAME");
-        if(username == null) {
-            username = (String) config.get("user");
-        }
-
-        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
-        if(accessKey == null) {
-            accessKey = (String) config.get("key");
         }
 
         if (capabilities.getCapability("bstack:options") != null) {
             HashMap bstackOptionsMap = (HashMap) capabilities.getCapability("bstack:options");
-            bstackOptionsMap.put("source", "testng:sample-selenium-4:v1.0");
-        } else {
-            JSONObject bstackOptions = new JSONObject();
-            bstackOptions.put("source", "testng:sample-selenium-4:v1.0");
-            capabilities.setCapability("bstack:options", bstackOptions);
-        }
-
-        this.checkAndStartBrowserStackLocal(capabilities, accessKey);
-
-        driver = new RemoteWebDriver(new URL("https://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
-    }
-
-    public void checkAndStartBrowserStackLocal(MutableCapabilities capabilities, String accessKey) throws Exception {
-        if (bsLocal != null) {
-            return;
-        }
-        if (capabilities.getCapability("bstack:options") != null
-                && ((JSONObject) capabilities.getCapability("bstack:options")).get("local") != null
-                && ((Boolean) ((JSONObject) capabilities.getCapability("bstack:options")).get("local")) == true) {
-            bsLocal = new Local();
-            Map<String, String> options = new HashMap<String, String>();
-            options.put("key", accessKey);
-            try {
-                bsLocal.start(options);
-            } catch (Exception e) {
-                System.out.println("Error: could not start browserstack local");
-                e.printStackTrace();
-                throw e;
+            if ((bstackOptionsMap.get("local") != null &&
+                    bstackOptionsMap.get("local").toString().equalsIgnoreCase("true") && (l == null || !l.isRunning()))) {
+                l = new Local();
+                Map<String, String> options = new HashMap<String, String>();
+                options.put("key", accessKey);
+                l.start(options);
             }
+            bstackOptionsMap.put("source", "testng:sample-selenium-4:v1.1");
         }
+
+        driver = new RemoteWebDriver(
+                new URL("http://" + username + ":" + accessKey + "@" + config.get("server") + "/wd/hub"), capabilities);
     }
 
-    @AfterMethod(alwaysRun=true)
-    public void tearDown() throws Exception {
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
         driver.quit();
-        if (bsLocal != null) {
-            bsLocal.stop();
-        }
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() throws Exception {
+        if (l != null) l.stop();
     }
 }
