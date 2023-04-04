05c37b7ccaf2a09a75a85414725be1a4db2af971
Kamalpreet Kaur <kmlkaur73@gmail.com>
2023-04-04 14:55:14 +0530

    chore: testing

--- src/test/java/com/browserstack/SeleniumTest.java
+++ src/test/java/com/browserstack/SeleniumTest.java
@@ -12,6 +12,7 @@ public class SeleniumTest {
     @BeforeMethod(alwaysRun = true)
     @SuppressWarnings("unchecked")
     public void setUp() throws Exception {
+        System.out.println("TESTING... PR");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
